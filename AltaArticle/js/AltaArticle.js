4a71ca919a1e2fb25ce4cd54495b52135139bfcc
jaumee99 <jcasamitjana8@boscdelacoma.cat>
2022-10-04 16:28:57 +0200

    fet

--- AltaArticle/js/AltaArticle.js
+++ AltaArticle/js/AltaArticle.js
@@ -1,14 +1,22 @@
-
 window.onload = function start(){
     AddFamilies();
-    //const input = document.querySelector('input');
-    //input.addEventListener('input', CodiRegEx());
+
+    document.getElementsByTagName("select")[0].addEventListener("change", comprovaCodi);
+    document.getElementById("codi").addEventListener("change", comprovaCodi);
+
+    document.getElementById("Passadis").addEventListener("change", comprovaPassadis);
+    document.getElementById("Estantaria").addEventListener("change", comprovaEstantaria);
+    document.getElementById("Forat").addEventListener("change", comprovaForat);
+
 }
 
 function AddFamilies(){
-    var Familia = ["Breach", "Ziggs", "Yummy", "Cypher", "Raze"];
+    var Familia = ['Breach', 'Ziggs', 'Yummy', 'Cypher', 'Raze'];
 
     let select = document.getElementById('Familia');
+
+    let mapFamilia = Familia.map(Families => Families.substr(0,3));
+    console.log(mapFamilia)
 
     Familia.sort();
     Familia.forEach (i => {
@@ -17,62 +25,72 @@ function AddFamilies(){
         opt.innerHTML = i;
         select.appendChild(opt);
     });
-
-/*    function CodiRegEx() {
-        let value = document.getElementById("Codi").value;
-        const regEx = /^[A-Za-z]{3}-\d{7}-[A | X | M | T | B | C | S | O | P | Z]$/;
-        return regEx.test(value);
-    }
-*/
-    const inputs = document.querySelectorAll('input');
-
-    const patterns = {
-        Codi: /^[A-Za-z]{3}-\d{7}-[A | X | M | T | B | C | S | O | P | Z]$/
-    }
-
-    function validate(field, regex){
-        if(regex.test(field.value)) {
-            field.className = 'valid';
-        } else{
-            field.className = 'invalid';
-        }
-    }
-
-    inputs.forEach((input) => {
-        input.addEventListener('keyup', (e) => {
-            console.log(e.target.value);
-            validate(e.target,patterns[e.target.attributes.name.value]);
-        });
-    });
-
-    function Img(inputElement, isValid) {
-        var Tick = "../imatges/tick.png";
-        var Creu = "../imatges/creu.png";
-        var imatge = inputElement.nextElementSibling;
-    
-        if (isValid) {
-            imatge.src = Tick;
-        } else {
-            imatge.src = Creu;
-    
-        }
-    
-    }
-
-
-    const Amplada = document.querySelector('#Amplada');
-    const Llargada = document.querySelector('#Llargada');
-    const Altura = document.querySelector('#Altura');
-    const result = document.querySelector('#result');
-    
-    Amplada.addEventListener('input', function () {
-        result.textContent = this.value;
-    });
-    Llargada.addEventListener('input', function () {
-        result.textContent = this.value;
-    });
-    Altura.addEventListener('input', function () {
-        result.textContent = this.value;
-    });
-    
 }
+
+const DC = new Map();
+    DC.set(0, "A");
+    DC.set(1, "X");
+    DC.set(2, "M");
+    DC.set(3, "T");
+    DC.set(4, "B");
+    DC.set(5, "C");
+    DC.set(6, "S");
+    DC.set(7, "O");
+    DC.set(8, "P");
+    DC.set(9, "Z");
+
+function comprovaCodi() {
+    var select = document.getElementsByTagName("select")[0];
+    var codi = document.getElementById("codi");
+
+    var digitsCodi = codi.value.split("-")[1];
+    var opcioEscollida = select.options[select.selectedIndex].text.toLowerCase();
+    var lletres = opcioEscollida.slice(0, 3);
+    var digitControl = DC.get(digitsCodi % 10);
+
+    var regEx = new RegExp(`^${lletres}`, "i", `-[0-9]{7}-${digitControl}{1}$`);
+    var regExpTest = regEx.test(codi.value);
+
+    Img(codi, regExpTest);
+}
+
+function comprovaPassadis() {
+    var codi = document.getElementById("Passadis");
+
+    var regEx = new RegExp(/^P-[0-9]{2}-(E|D){1}$/);
+    var regExpTest = regEx.test(codi.value);
+
+    Img(codi, regExpTest);
+}
+
+function comprovaEstantaria() {
+    var codi = document.getElementById("Estantaria");
+
+    var regEx = new RegExp(/^EST\+[0-9]{2}\.[0-9]{2}$/);
+    var regExpTest = regEx.test(codi.value);
+
+    Img(codi, regExpTest);
+}
+
+function comprovaForat() {
+    var codi = document.getElementById("Forat");
+
+    var regEx = new RegExp(/^[0-9]{2}\*[A-Z]{3}\*[0-9]{2}\[0-9]{2}$/);
+    var regExpTest = regEx.test(codi.value);
+
+    Img(codi, regExpTest);
+}
+
+function Img(inputElement, isValid) {
+    var Tick = "./imatges/tick.png";
+    var Creu = "./imatges/creu.png";
+    var imatge = inputElement.nextElementSibling;
+
+    if (isValid) {
+        imatge.src = Tick;
+    } else {
+        imatge.src = Creu;
+    }
+}
+
+
