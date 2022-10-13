15aa7763bc960e7bd625dfd49927ac38e8324a1b
jaumee99 <jcasamitjana8@boscdelacoma.cat>
2022-10-13 18:46:12 +0200

    estrelles random

--- Estrelles/js/index.js
+++ Estrelles/js/index.js
@@ -1,3 +1,56 @@
+//estrelles
+const backgroundColor = "#000";
+const width = window.innerWidth;
+const height = window.innerHeight;
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = width;
+canvas.height = height;
+
+function randomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function fillCircle(ctx, x, y, r, fillStyle) {
+    ctx.beginPath();
+    ctx.fillStyle = fillStyle;
+    ctx.arc(x, y, r, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+const maxStarRadius = 1.5;
+
+function createStars(width, height, spacing) {
+    const stars = [];
+
+    for (let x = 0; x < width; x += spacing) {
+        for (let y = 0; y < height; y += spacing) {
+        const star = {
+            x: x + randomInt(spacing),
+            y: y + randomInt(spacing),
+            r: Math.random() * maxStarRadius,
+        };
+        stars.push(star);
+        }
+    }
+    return stars;
+}
+
+const stars = createStars(width, height, 30);
+
+function render() {
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, width, height);
+    stars.forEach(function(star) {
+        const x = star.x;
+        const y = star.y;
+        const r = star.r;
+        fillCircle(ctx, x, y, r, "rgb(255, 255, 255)");
+    });
+}
+
+render();
+
 
 //html input range i localStorage
 function numeroSTR(val) {
