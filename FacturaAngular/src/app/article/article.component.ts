d2eafeff235670cb25683d451f0afd9e86d91948
jaumee99 <jcasamitjana8@boscdelacoma.cat>
2022-11-10 16:57:22 +0100

    avances

--- FacturaAngular/src/app/article/article.component.ts
+++ FacturaAngular/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Producte } from "./producte";
 
 @Component({
   selector: 'app-article',
@@ -8,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class ArticleComponent implements OnInit {
 
   constructor() { }
+  seleccionat?: Producte;
+
+  productes:Producte[] = [
+    {codi: 1, nom: "arroz", quantitat: 10, preu: 12, total: 11},
+    {codi: 2, nom: "pa", quantitat: 10, preu: 5, total: 11},
+    {codi: 3, nom: "oli", quantitat: 10, preu: 8, total: 11},
+    {codi: 4, nom: "sucre", quantitat: 10, preu: 22, total: 11},
+    {codi: 5, nom: "verd", quantitat: 10, preu: 42, total: 11},
+  ];
 
   ngOnInit(): void {
   }
 
+
+  addItem() {
+    alert(this.seleccionat?.codi);
+  }
 }
---
+++ FacturaAngular/src/app/article/producte.ts
@@ -0,0 +1,7 @@
+export interface Producte {
+    codi: number;
+    nom: string;
+    quantitat: number;
+    preu: number;
+    total: number;
+}
